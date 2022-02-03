bb5088a99243bcd384868f83c2a779fce00430d1
wojtazik <wojtas098@gmail.com>
2022-02-03 13:08:32 +0100

    Fix date

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ export default function Home({ allPostsData }) {
                             </Link>
                             <br/>
                             <small className={utilStyles.lightText}>
-                                <Date dateString={{date}} />
+                                <Date dateString={date} />
                             </small>
                         </li>
                     )) }
